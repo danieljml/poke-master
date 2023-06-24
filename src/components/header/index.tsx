9c1005f8ff970bccd57a0c790878191fde724fd3
danieljml <danielgarrido425@gmail.com>
2023-06-24 13:02:16 -0400

    Adding navigation dashboard on header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -22,6 +22,11 @@ const Header = () => {
     localStorage.removeItem('user')
     navigate('/login')
   }
+
+  const handleNavigationDashboard = () => {
+    navigate('/')
+  }
+
   return (
     <header className="bg-green">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
@@ -42,9 +47,9 @@ const Header = () => {
           </button>
         </div>
         <Popover.Group className="hidden lg:flex lg:gap-x-12">
-          <p className="text-sm font-semibold leading-6 text-gray-900">
+          <button onClick={handleNavigationDashboard} className="text-sm font-semibold leading-6 text-gray-900">
             DASHBOARD
-          </p>
+          </button>
         </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
           <button onClick={handleLogout} className="text-sm font-semibold leading-6 text-gray-900">
@@ -76,11 +81,11 @@ const Header = () => {
           <div className="mt-6 flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
-                <p
+                <button onClick={handleNavigationDashboard} 
                   className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   DASHBOARD
-                </p>
+                </button>
               </div>
               <div className="py-6">
                 <button
