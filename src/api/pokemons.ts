aa47c3abf45f52a144f67b2a7fcde66285af3bd0
danieljml <danielgarrido425@gmail.com>
2023-06-23 20:54:29 -0400

    Refactor Pokemon API requests

--- src/api/pokemons.ts
+++ src/api/pokemons.ts
@@ -5,19 +5,57 @@ interface PokemonResult {
   url: string;
 }
 
-interface PokemonResponse {
-  results: PokemonResult[];
+interface PokemonAbility {
+  ability: {
+    name: string;
+  };
 }
 
-export const getPokemons = async (currentPage: number): Promise<PokemonResponse | undefined> => {
+interface PokemonSprites {
+  front_default: string;
+}
+
+interface PokemonDetails {
+  name: string;
+  image: string;
+  height: number;
+  skills: string[];
+}
+
+export const getPokemons = async (currentPage: number): Promise<PokemonDetails[] | undefined> => {
   try {
-    const response: AxiosResponse<PokemonResponse> = await axios.get('https://pokeapi.co/api/v2/pokemon', {
+    const response: AxiosResponse<{ results: PokemonResult[] }> = await axios.get('https://pokeapi.co/api/v2/pokemon', {
       params: {
         limit: 10,
         offset: (currentPage - 1) * 10,
       },
     });
-    return response.data;
+
+    const { results } = response.data;
+
+    const pokemonDetailsPromises = results.map(async (pokemon: PokemonResult) => {
+      const pokemonResponse: AxiosResponse<{
+        name: string;
+        sprites: PokemonSprites;
+        height: number;
+        abilities: PokemonAbility[];
+      }> = await axios.get(pokemon.url);
+
+      const { name, sprites, height, abilities } = pokemonResponse.data;
+      const skills = abilities.map((ability) => ability.ability.name);
+      const image = sprites.front_default;
+
+      return {
+        name,
+        image,
+        height,
+        skills,
+      };
+    });
+
+    const pokemonDetails: PokemonDetails[] = await Promise.all(pokemonDetailsPromises);
+
+    return pokemonDetails;
   } catch (error) {
     console.log('Error fetching Pokemon:', error);
   }
